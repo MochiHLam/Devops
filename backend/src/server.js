776201fe2968e1e6e624f12905a3627b38e14d2e
Nguyen Hoang Lam <nhoanglam2309@gmail.com>
2026-08-04 18:07:12 +0700

    feat: change server.js to test cd pipeline

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello from Docker!");
+    res.send("Hello DevOps CI/CD!!!");
 });
 
 app.listen(PORT, () => {
